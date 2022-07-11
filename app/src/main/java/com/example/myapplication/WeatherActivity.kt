29ab4e72c9496c66529e85222e51149d6e4099a6
artem <artem_mikhaylov_1994@bk.ru>
2022-07-11 14:36:29 +0300

    - добаылен экран WindActivity.kt

--- app/src/main/java/com/example/myapplication/WeatherActivity.kt
+++ app/src/main/java/com/example/myapplication/WeatherActivity.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.feature.weather_screen.ui.WindActivity
 import java.util.*
 
 class WeatherActivity : AppCompatActivity(){
@@ -13,6 +16,11 @@ class WeatherActivity : AppCompatActivity(){
         setContentView(R.layout.activity_weather)
 
         val tvTemperature = findViewById<TextView>(R.id.tvTemperature)
-        tvTemperature.text = weatherPresenter.getWeather(UUID.randomUUID().toString())
+        tvTemperature.text = weatherPresenter.getWeather()
+
+        val btnWind = findViewById<Button>(R.id.btnWind)
+        btnWind.setOnClickListener {
+            Intent(this, WindActivity::class.java).also(::startActivity)
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/feature/weather_screen/ui/WindActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.feature.weather_screen.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+
+class WindActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wind)
+
+    }
+}
